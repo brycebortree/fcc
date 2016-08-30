2bdba302ce94b4252f1b584c5e90e30130e5aad5
brytree <brycebortree@gmail.com>
2016-08-30 15:59:38 -0700

    incorporating filter in diff

--- bonfires/diffarrays.js
+++ bonfires/diffarrays.js
@@ -31,3 +31,35 @@ function diffArray(arr1, arr2) {
 }
 
 diffArray([1, "calf", 3, "piglet"], [7, "filly"]);
+
+//working with filter/concat.... learning filter
+function diffArray(arr1, arr2) {
+  var long, short;
+  var garArr = [];
+  var newArr = [];
+  
+  if(arr1.length > arr2.length){
+    long = arr1;
+    short = arr2;
+  } else {
+    long = arr2;
+    short = arr1;
+  }
+  
+  for(var i = 0; i < long.length + short.length; i++){
+    if(long.indexOf(short[i]) != -1){
+    garArr.push(long.slice(long.indexOf(short[i]), long.indexOf(short[i]) + 1));
+    }
+  }
+  
+  short.filter(function(arr){
+    return 
+    
+  });
+    
+  newArr = long.concat(short);
+  
+  return garArr;
+}
+
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);

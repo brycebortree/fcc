a550dffce43d922fe8bca9643f80f4819b0ec827
brytree <brycebortree@gmail.com>
2016-08-31 12:49:56 -0700

    styling up

---
+++ quotemachine/script.js
@@ -0,0 +1,29 @@
+.bg{ 
+  background-image:url(http://41.media.tumblr.com/73413647e38f24f49f9dc2f65a49a2e4/tumblr_mqmznfyEOz1svt1nso1_1280.jpg);
+  background: stretch;
+  background-repeat: repeat;
+}
+
+.body {
+  font-family: helvetica;
+  color: black;
+  font-weight: bold;
+}
+
+#display {
+  font-family: Typewriter, Courier, serif;
+  color: LawnGreen;
+  font-size: 20px;
+  height: 200px;
+  width: 500px;
+  background-color:black;
+  margin: 0 auto;
+}
+
+#quote {
+  align: center;
+}
+
+#clickEvent {
+  margin-top: 150px;
+}

4101e67a634fd3f875c8760165ea8c0d8c2c974c
brytree <brycebortree@gmail.com>
2016-08-31 12:50:44 -0700

    basic functionality up

--- quotemachine/script.js
+++ quotemachine/script.js
@@ -1,29 +1,28 @@
-.bg{ 
-  background-image:url(http://41.media.tumblr.com/73413647e38f24f49f9dc2f65a49a2e4/tumblr_mqmznfyEOz1svt1nso1_1280.jpg);
-  background: stretch;
-  background-repeat: repeat;
-}
+var digiRay = [
+  "Sora Takenouchi: Too bad your brain isn't as big as your hair.",
+  "Tentomon: The name's Mon... Tento Mon.",
+  "Matt: I've been living a lie.",
+  "Gabumon: You're not a natural blonde?",
+  "Kari: What could be worse than living your life without a single friend?",
+  "Gatomon: A litter box full of fly paper.",
+  "Sora: Now that you boys have holes in your heads, maybe your brains will get more oxygen.",
+  "Recording on telephone: This number only exists in your imagination. Please hang up and don't call back.",
+  "T.K.: What's more boring? Paint drying or math?",
+  "Leomon: [dying] Why... can't you see? Why won't you try to understand the truth?",
+  "Joe: I wanted to go to summer school but would my parents listen to me? NO.",
+  "Recording over telephone: At the tone, the time will be exactly 45 miles per hour and 90 seconds.",
+  "Ken: [reading] Dear Ken, I hope that someday you'll join the Digidestined. Signed Yolei."
+]
 
-.body {
-  font-family: helvetica;
-  color: black;
-  font-weight: bold;
-}
 
-#display {
-  font-family: Typewriter, Courier, serif;
-  color: LawnGreen;
-  font-size: 20px;
-  height: 200px;
-  width: 500px;
-  background-color:black;
-  margin: 0 auto;
-}
+var click = document.getElementById("clickEvent");
 
-#quote {
-  align: center;
-}
+click.addEventListener('click', function(){
+  var rand = digiRay[Math.floor(Math.random() * digiRay.length)];
 
-#clickEvent {
-  margin-top: 150px;
-}
+  console.log(rand); document.getElementById("quote").innerHTML = rand;
+});
+
+
+
+

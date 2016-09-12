1be5715fcabc053263c4d1420eca1661290c51f3
brytree <brycebortree@gmail.com>
2016-09-12 15:27:12 -0700

    beginning rom num

--- bonfires/int/roman.js
+++ bonfires/int/roman.js
@@ -1,6 +1,16 @@
 
 function convertToRoman(num) {
- return num;
+ var roman = ["I", "IV","V", "VI", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+ var decimal = [1, 4, 5, 6, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  
+ var numToRom = '';
+  
+ for(var i = 0; i < num.length; i++){
+   
+ }
+ 
+  
+ return numToRom;
 }
 
 convertToRoman(36);

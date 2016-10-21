137d0ef158d7a1e700edb060b23f698ac362e57b
brytree <brycebortree@gmail.com>
2016-10-21 09:40:43 +0100

    add getIndex

---
+++ bonfires/beg/getIndex.js
@@ -0,0 +1,20 @@
+//learned compare portion!!
+function getIndexToIns(arr, num) {
+  arr = arr.sort(function(a, b){
+    return a - b;
+  });
+  var i = 0;
+  
+  while(i < arr.length){
+    if(num > arr[i]){
+      i++;
+    } else {
+      break;
+    }
+  }
+  return i;
+}
+
+
+
+getIndexToIns([40, 60], 50);
